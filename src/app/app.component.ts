71110c7a0b31deb8899735cabf14fa2af8ade14e
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-09 19:17:36 +0530

    http get and post done, working well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +11,48 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchData();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post('https://http-request-tutorial-udemy-default-rtdb.firebaseio.com/posts.json', postData)
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchData();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchData() {
+    this.http.get<{[key: string]: Post}>('https://http-request-tutorial-udemy-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(
+        responseData => {
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        }
+      ))
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        }
+      )
   }
 }
