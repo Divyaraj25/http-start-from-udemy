8adc7a73efde0d0e2fd666709c97c3034cba9460
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-10 11:50:33 +0530

    logging interceptor added, multiple interceptors at once

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { loggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,14 @@ import { AuthInterceptorService } from './auth-interceptor.service';
     FormsModule,
     HttpClientModule
   ],
+  // order of interceptors matters here
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptorService,
+    multi: true
+  },{
+    provide: HTTP_INTERCEPTORS,
+    useClass: loggingInterceptorService,
     multi: true
   }],
   bootstrap: [AppComponent]
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,20 +3,12 @@ import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler){
-        console.log(req.url);
         const modifiedReq = req.clone({
             headers: req.headers.append('Auth', 'xyz'),
             params: req.params.append('interceptor', 'true')
         })
         console.log(modifiedReq.urlWithParams);
         console.log('request is on its way');
-        return next.handle(modifiedReq).pipe(tap(event => {
-            console.log(event);
-            if(event.type === HttpEventType.Response){
-                console.log(event.body);
-                console.log(event.type);
-                console.log('response received');
-            }
-        }))
+        return next.handle(modifiedReq)
     }
 }
